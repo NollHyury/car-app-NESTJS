8c99547d5f83539ef5b9b438c99297fba5ec35c4
Hyury Luis da Silva Noll <hyury@weg.net>
2022-06-09 17:13:33 -0300

    working

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Post,
   Query,
   NotFoundException,
-  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dto/create-user.dto';
