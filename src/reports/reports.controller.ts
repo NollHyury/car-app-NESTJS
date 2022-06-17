2c94a089df329390df59c4d2227b50897607312a
Hyury Luis da Silva Noll <hyury@weg.net>
2022-06-17 09:39:41 -0300

    add authorization guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,14 +1,35 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { AuthGuard } from '../guards/auth.guard';
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
+import { User } from 'src/users/user.entity';
+import { ReportDto } from './dto/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dto/approve-report.dto';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approved(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    const { approved } = body;
+
+    return this.reportsService.changeApproval(id, approved);
   }
 }
